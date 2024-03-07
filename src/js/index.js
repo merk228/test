2ffce60d04c1511467eb8b8b86b4c18c4bb4ae08
merk <gettefaust240@gmail.com>
2024-03-07 12:58:21 +0300

    commit for commits

--- src/js/index.js
+++ src/js/index.js
@@ -1,63 +1,50 @@
-let body = document.querySelector("body")
-let birgi = document.querySelectorAll(".spec-item")
-let podlist = document.querySelector(".header__pod-list")
-let headerDecor = document.querySelector(".header__item_decor")
-let dropList = document.querySelector(".header__list")
-let close = document.querySelector(".header__close")
-
-document.addEventListener("click", dropMenu)
-
-function dropMenu(event) {
-  if (event.target.closest('.spec-item')) {
-    podlist.classList.toggle('podlist-active')
-    headerDecor.classList.toggle('headerDecor-active')
-  }
-
-  else {
-    podlist.classList.remove('podlist-active')
-    headerDecor.classList.remove('headerDecor-active')
-  }
-
-  if (event.target.closest('.header__burger-menu')) {
-    dropList.classList.toggle('header__list-active')
-    body.classList.toggle('body_hidden')
-  } else if
-    (!event.target.closest('.header__list')) {
-    dropList.classList.remove('header__list-active')
-    body.classList.remove('body_hidden')
-  }
-
-  if (event.target.closest('.header__close')) {
-    dropList.classList.remove('header__list-active')
-    body.classList.remove('body_hidden')
-  }
-
-}
-
-
-// swiper-slider
-
 import Swiper from 'swiper/bundle';
 import 'swiper/css/bundle';
-
-let swiper = new Swiper('.myswiper', {
-  // Опции слайдера
-
+const swiper = new Swiper('.bloc2__swiper', {
   // Optional parameters
-  direction: 'horizontal',
   slidesPerView: 1,
   loop: true,
-  speed: 500,
+  parallax: true,
+  speed: 400,
+  grabCursor: true,
   keyboard: {
     enabled: true,
   },
 
 
+  // If we need pagination
+  pagination: {
+    el: '.fraction',
+    type: 'fraction',
+    clickable: true,
+  },
+
   // Navigation arrows
   navigation: {
     nextEl: '.button-next',
     prevEl: '.button-prev',
-  },
-  loop: true,
+  }
 
-}); 
+});
+
+let OpenBtn = document.querySelector(".header__btn");
+let Openclose = document.querySelector(".header__btn-close");
+let navOpen = document.querySelector(".nav-mob")
+let hidden = document.querySelector("body")
+
+document.body.addEventListener('click', oneclick);
+
+function oneclick(event) {
+  if (event.target.closest('.header__btn')) {
+    navOpen.classList.toggle('nav-active')
+    OpenBtn.classList.toggle('btn-active')
+    Openclose.classList.toggle('btn-closeactive')
+    hidden.classList.toggle('hidden')
+  }
+  else if (!event.target.closest('.nav-active')) {
+    navOpen.classList.remove('nav-active')
+    OpenBtn.classList.remove('btn-active')
+    Openclose.classList.remove('btn-closeactive')
+    hidden.classList.remove('hidden')
+  }
+}
